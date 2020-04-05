fdca64aadd1e432c3709d69242db7c6d310e536d
Jacob Zirbel <jzerble@gmail.com>
2020-04-04 21:54:03 -0500

    generate DOM with js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,29 +1,42 @@
-let items = "cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,".split(
-	","
-);
-let container = document.getElementsByClassName("container")[0];
+let items = "cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird,cat,dog,fish,deer,bird".split(
+		","
+	),
+	container,
+	topRow;
+
+let initialSetup = () => {
+	container = document.getElementsByClassName("container")[0];
+	topRow = document.createElement("div");
+	let inputF = document.createElement("div");
+	inputF.setAttribute("class", "col-md-3  ml-2 mr-1");
+	inputF.setAttribute("id", "new-item-input");
+	let title = document.createElement("h6");
+	title.textContent = "Add a search term";
+	let textField = document.createElement("INPUT");
+	textField.setAttribute("type", "text");
+	let submitButton = document.createElement("INPUT");
+	submitButton.setAttribute("type", "submit");
+	submitButton.setAttribute("class", "mt-2 btn-secondary");
+	inputF.append(title, textField, submitButton);
+	topRow.appendChild(inputF);
+	container.appendChild(topRow);
+	topRow.setAttribute("class", "row");
+	generateButtons(items);
+};
 let generateButtons = (items) => {
-	let buttonsContainer = document
-		.createElement("div")
-		.setAttribute("class", "container buttons");
-	let row;
+	let buttonsCol = document.createElement("div");
+	buttonsCol.setAttribute("class", "col-md-8 buttons");
 	items.forEach((e, i, a) => {
-		if (i % 6 === 0) {
-			row = document.createElement("div");
-			row.setAttribute("class", "row justify-content-start");
-		}
 		let button = document.createElement("button");
-		button.setAttribute("class", "col-md-2 btn btn-primary");
+		button.setAttribute("class", "btn btn-primary");
 		button.textContent = e;
 		button.onclick = () => getGifs(e);
-		row.appendChild(button);
-		if (i % 6 === 0 || i === a.length - 1) {
-			container.appendChild(row);
-		}
+		buttonsCol.appendChild(button);
 	});
+	topRow.insertBefore(buttonsCol, document.getElementById("new-item-input"));
 };
 
 let getGifs = (item) => {
 	console.log(item);
 };
-generateButtons(items);
+initialSetup();
